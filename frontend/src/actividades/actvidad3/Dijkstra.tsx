e1a9cedef0527f07b02d8cfd96e5a5ab09ab8559
Salvador Gomez <darklord@sin-nombre.com>
2025-04-27 15:40:21 -0600

    Update Dijkstra.tsx

--- frontend/src/actividades/actvidad3/Dijkstra.tsx
+++ frontend/src/actividades/actvidad3/Dijkstra.tsx
@@ -5,8 +5,8 @@ import { json } from 'react-router-dom';
 export const Dijkstra = () => {
 
     const MaxWeight = 30;
-    const nodos = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-
+     const nodos = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    //const nodos = ['0', '1', '2', '3', '4', '5', '6','7'];
     //generar matriz de adyacencia aleatoria no generar retornos entre nodos
     const generateRandomMatrix = (rows: number, cols: number) => {
         const matrix = [];
@@ -45,7 +45,11 @@ export const Dijkstra = () => {
                     {
                         if(cointToss())
                         {
-                            row.push(Math.floor(Math.random() * MaxWeight));
+                            //evitar conexion entre primer y ultimo nodo
+                            if(i==0 && rows-1==j)
+                                row.push(0);
+                            else
+                                row.push(Math.floor(Math.random() * MaxWeight));
                         }
                         else
                         {
@@ -60,26 +64,163 @@ export const Dijkstra = () => {
             }
             matrix.push(row);
         }
+        console.log(matrix);
         return matrix;
     }
 
     //matriz de adyacencia
     const [matriz, setMatriz] = useState([
-        [0, 8, 3, 0, 0],
-        [0, 0, 5, 5, 50],
-        [0, 0, 0, 4, 5],
-        [0, 0, 0, 0, 1],
-        [0, 0, 0, 0, 0]
+        [0, 2, 0, 0, 0, 29, 0],
+        [0, 0, 20, 15, 0, 20, 0],
+        [0, 0, 0, 0, 18, 29, 5],
+        [0, 0, 0, 0, 24, 24, 10],
+        [0, 0, 0, 0, 0, 0, 50],
+        [0, 0, 0, 0, 0, 0, 27],
+        [0, 0, 0, 0, 0, 0, 0]
     ]);
 
-    //
+    //convertir
+
+
+    //algoritmo de dijkstra
+
+    const min = (distancia: number[], visitado: boolean[]) => {
+        let min = Infinity;
+        let minIndex = -1;
+
+        for(let v=0; v<distancia.length; v++)
+        {
+            if(!visitado[v] && distancia[v] <= min)
+            {
+                min = distancia[v];
+                minIndex = v;
+            }
+        }
+
+        return minIndex;
+    }
+
+
+    const DijkstraAlgorithm = (matriz: number[][], nodoInicial: number) => {
+        const distancia = [];
+        const visitado = [];
+        const paths = [];
+
+        for(let i=0; i<matriz.length; i++)
+        {
+            distancia[i] = Infinity;
+            visitado[i] = false;
+        }
+
+        distancia[nodoInicial] = 0;
+
+        
+
+        for(let i=0; i<matriz.length-1; i++)
+        {
+
+            const path = [];
+            const u = min(distancia, visitado);
+            visitado[u] = true;
+
+            for(let v=0; v<matriz.length; v++)
+            {
+                if(!visitado[v] && matriz[u][v] != 0 && distancia[u] != Infinity && distancia[u] + matriz[u][v] < distancia[v])
+                {
+                    distancia[v] = distancia[u] + matriz[u][v];
+                }
+
+            }
+            // path.push(u);
+            paths.push( {path, distancia: distancia[u], nodo: i,cosa:u});
+        }
+
+        // console.log(distancia);
+        // console.log("paths",JSON.stringify(paths, null, 2));
+
+        return {distancia, paths};
+    }
+
+//converit matriz de adyacencia a lista de adyacencia en duplas
+const matrizToLista = (matriz: number[][]) => {
+   const lista = [];
+   for(let i=0; i<matriz.length; i++)
+   {
+    const row = [];
+      for(let j=0; j<matriz[i].length; j++)
+      {
+        if(matriz[i][j] > 0)
+        row.push({[nodos[j-1]]: matriz[i][j]});
+      }
+      //copnveritr arrary a objeto
+      const obj = {};
+      console.log(row);
+      for(let j=0; j<row.length; j++)
+      {
+        obj[row[j].key] = row[j].value;
+      }
+
+      const nodo = nodos[i];
+      lista.push({[nodo]:obj});
+   }
+   return lista;
+}
+
+   // algormo dijstra entre dos nodos
+//    const DijkstraBetweenNodes = (matriz: number[][], nodoInicial: number, nodoFinal: number) => {
+//         let distancias={}
+//         let anteriores={}
+        
+//         for (let nodo in matriz)
+//        {
+//         if(nodo !== nodoInicial)
+//             {distancias[nodo]=Infinity}
+//         anteriores[nodo]=null
+//        }
+
+//        let porVisitar = matriz
+    
+//    }
+
+
+// algoritmo de dijkstra entre dos nodos
+const DijkstraBetweenNodes = (matriz: number[][], nodoInicial: number, nodoFinal: number) => {
+
+    let distancias={}
+    let anteriores={}
+
+    for(let nodo in matriz)
+    {
+        if(nodo !== nodoInicial)
+        {distancias[nodo]=Infinity}
+        anteriores[nodo]=null
+    }
+
+    let porVisitar = matriz
+
+    while(nodoInicial !== nodoFinal)
+    {
+        let nodoActual = nodoInicial;
+    }   
+
+    return {distancias, anteriores};
+}
+
+
+       
+
 
 
 
     //convertir matriz de adyacencia a lista de adyacencia en duplas
-    const lista = matriz.map((row, i) =>
-        row.map((col, j) => ({ source: nodos[i], target: nodos[j], weight: col }))
-    );
+    const grafo = matriz.map((row, i) =>
+        row.map((col, j) => 
+            {
+
+                return { source: nodos[i], target: nodos[j], weight: col }
+            
+        }
+        ))
 
     //Renderizar la matriz de adyacencia
     const RenderMatriz = ({ matriz }: { matriz: number[][] }) => {
@@ -96,7 +237,6 @@ export const Dijkstra = () => {
 
     //Generar grafo en dot desde lista de adyacencia
     const LitsToDot = (listaNodos: { source: string, target: string, weight: number }[][]) => {
-        console.log(JSON.stringify(listaNodos, null, 2));
         const dot = `digraph G {
         rankdir=LR;
         node [shape=circle];
@@ -112,18 +252,14 @@ export const Dijkstra = () => {
         return dot;
     };
 
-
-
-
-
-
-    console.log(JSON.stringify(lista, null, 2));
-    console.log(LitsToDot(lista));
+console.log("grafo",  JSON.stringify(grafo, null, 2));
+//  console.log("lista",  JSON.stringify(matrizToLista(matriz), null, 2));
+// console.log("dijkstra",  JSON.stringify(DijkstraBetweenNodes(matriz, 0, 6), null, 2));
 
     return (
         <div className='items-center justify-center flex-1 w-full h-full bg-gray-400'>
             <button
-                onClick={() => setMatriz(generateRandomMatrixNoRetorno(5, 5))}
+                onClick={() => setMatriz(generateRandomMatrixNoRetorno(7, 7))}
                 className='justify-center flex-1 p-3 m-2 text-white bg-blue-800 rounded-xl hover:bg-blue-900'
             >
                 Generar matriz aleatoria
@@ -134,9 +270,18 @@ export const Dijkstra = () => {
                 </div>
                     <div className='p-3 m-2 overflow-hidden bg-white rounded shadow-lg'>
                     {/* //grafico debe ser maximo 300 px ancho */}
-                    <Graphviz dot={LitsToDot(lista)} />
+                    <Graphviz dot={LitsToDot(grafo)} />
+                    <div className='p-3 m-2 overflow-hidden bg-white rounded shadow-lg'>
+                       {DijkstraAlgorithm(matriz,0).distancia.map((distancia, i) => (
+                        <div key={i}>
+                            {nodos[i]}: {distancia}
+                        </div>
+                       ))}
+                      
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
+
